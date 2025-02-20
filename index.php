a21872b62104361c3b5ee9c481fa2776e9d2dbe9
Jordi <jotolo1252@gmail.com>
2025-02-20 12:28:58 +0100

    subida1

--- index.php
+++ index.php
@@ -8,12 +8,38 @@
     <script src="script.js"></script>
 </head>
 <body>
+
+<form action="">
+    <h1>Inicio de sesión</h1>
+    <label for="usuario">Usuario</label>
+    <input type="text" name="usuario" id="usuario" placeholder="Usuario">
+    <label for="password">Contraseña</label>
+    <input type="password" name="password" id="password" placeholder="Contraseña">
+    <input type="submit" value="Iniciar sesión">
+    <a href="registro.php">Registrarse</a>
+    <a href="recuperar.php">Recuperar contraseña</a>
+</form>
     
 </body>
 </html>
 
-<?php 
+<?php
 
+    $usuario = $_POST['usuario'];
+    $password = $_POST['password'];
 
+    $consulta = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND password = '$password'";
+    $resultado = mysqli_query($conexion, $consulta);
+
+    $filas = mysqli_num_rows($resultado);
+
+    if($filas > 0){
+        header("location:inicio.php");
+    }else{
+        echo "Error en la autenticación";
+    }
+
+    mysqli_free_result($resultado);
+    mysqli_close($conexion);
 
 ?>
